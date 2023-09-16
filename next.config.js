66e9aac67bc58862b3dc16e8d3b1a2dcf5e6f23e
javanance <javanance@gmail.com>
2023-09-17 02:37:15 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: "export",
+  reactStrictMode: true,
+  swcMinify: true,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
